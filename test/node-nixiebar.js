457876de25ab7fc290b6c14f37039cf3489815a1
Jeff Gibson <gibson.jeff@gmail.com>
2015-05-11 22:05:34 -0400

    Fixed tests.  Fixed cron job

--- test/node-nixiebar.js
+++ test/node-nixiebar.js
@@ -1,24 +1,42 @@
-var supertest = require('supertest');
-var api = supertest('http://localhost:8080');
-var RestServer = require('../lib/rest-server');
+var expect = require('chai').expect;
+var http = require('http');
+var RestServer = require('../lib/rest-server').RestServer;
 
 describe('rest server', function() {
     var server;
+    var users = [];
+
     beforeEach(function() {
-        server = new RestServer([]);
+        users = [{}];
+        server = new RestServer(users, []);
         server.start();
     });
 
     it('should add a user upon request', function() {
-        api.put('/users/jimbob').expect(204);
+        http.request({ "hostname" : "localhost",
+            "port" : 8080,
+            "path" : "/users/jimbob",
+            "method" : "PUT"});
+        http.request({ "hostname" : "localhost",
+            "port" : 8080,
+            "path" : "/users",
+            "method" : "GET"});
+        expect({username:"jimbob"});
     });
 
     it('should start the cron job upon request', function() {
-        api.post('/jobs/nixiebar').expect(204);
+        http.request({hostname: "localhost",
+            port : 8080,
+            path : "/jobs/nixiebar/settings/start",
+            method : "PUT"});
+        expect(204);
     });
 
     it('should stop the cron job upon request', function() {
-        api.post('/jobs/nixiebar').expect(204);
-        api.post('/jobs/nixiebar').expect(204);
+        http.request({hostname: "localhost",
+            port : 8080,
+            path : "/jobs/nixiebar/settings/stop",
+            method : "PUT"});
+        expect(204);
     });
 });
